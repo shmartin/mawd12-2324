f568b7823c5e1e8414e90751df9a4163731c3a0d
Juan Martin de la Cruz Abad <75934242+shmartin@users.noreply.github.com>
2023-10-23 15:31:30 +0800

    Uploaded first demo files

---
+++ script.js
@@ -0,0 +1,185 @@
+//document.write("external");
+///*
+//document.write("internal");
+//document.write("internal");
+//document.write("internal");
+//document.write("internal");
+//*/
+////single line comment
+//document.write("internal");
+//
+//
+//var lando = "saksak";
+//var number1 = 10;
+//
+//document.writeln(lando);
+//document.writeln("\\"+number1);
+//
+//var letterfirstbeforenumber = true
+//
+///*
+//*** String Variables ***
+//\` - quote
+//\" - double quote
+//\\ - backslash
+//\n - new line
+//\r - carriage return / enter
+//\t - tab
+//\b - backspace
+//\f - form feed / page break
+//*/
+//
+//
+///*
+//+ - addition
+//- - subtraction
+//* - multiplication
+/// - division
+//% - modulo / remainder
+//++ - increment
+//-- - decrement
+//
+//var++ - post increment
+//++var - pre increment
+//
+//= - assigns a value to a var
+//+= - equivalent to x = x + y
+//-= - equivalent to x = x - y
+//*= - equivalent to x = x * y
+///= - equivalent to x = x / y
+//%= - equivalent to x = x % y
+//
+//
+//== - equal to
+//=== - "1" === 1 is false
+//!= - not equal to
+//!== - "1" !== 1 is true
+//> - greater than
+//>= - greater than equal to
+//< - lesser than
+//<= - lesser than equal to
+//
+//&& 
+//||
+//!
+//
+//*/
+//document.write("<br>");
+//var a = 0, b = 10;
+//var a = ++b;
+//document.write(a+" "+b);
+//
+//document.write("<br>");
+//var x = 5, y = 10;
+//x-=y;
+//document.write(x);
+
+
+
+
+
+
+var jname0 = "strings";
+var jname = 1;
+var jname = false;
+var jname = 2.3;
+var jname0 = 'd';
+
+// rules in naming variables.
+// - it should not have special characters.
+// - it should not start with a number.
+// - it should not be a keyword; ex: if, while, function, do, else, etc.
+// - it should not have spaces.
+
+var hello_world0 = 1;
+
+let hello_world1 = 2;
+
+//document.write(hello_world1);
+
+//if else, while, do while, switch.
+//for, while, do while.
+//and, or, not, xor
+
+var checker1 = true;
+var checker2 = false;
+
+if (!checker1 && checker2 || false || true && !true){
+    document.writeln("It is true");
+    if (true){
+        document.writeln("nested if")
+    }
+} else if (checker1 && checker2){
+    document.writeln("it is false");
+} else {
+
+}
+
+//var i = 0;
+//
+//while (i <= 10){
+//    document.writeln(i);
+//    i++;
+//}
+//
+//do {
+//    document.writeln(i);
+//    i++;
+//} while(i <= 10)
+
+//var month = "february";
+//
+//switch (month){
+//    case "january":
+//        document.writeln("1");
+//        //break;
+//    case "february":
+//        document.writeln("2");
+//        //break;
+//    default:
+//        document.writeln("not a month");
+//}
+
+
+
+
+
+var neil = 3;
+var prince_pogi = true;
+ 
+if (neil == 3 || !prince_pogi){
+    document.writeln("hello barney");
+
+}
+else{
+    document.writeln("hello kokey");
+}
+
+
+
+
+do {
+    document.writeln("hello ken");
+}while(false);
+
+while(false){
+    document.writeln("hi ken");
+}
+
+
+
+
+
+
+
+var initial = 'C' ;
+switch (initial){
+    case 'B':
+        document.writeln("Barney");
+        break;
+    case 'C':
+        document.writeln("Charley") ;
+        break;
+    default:
+        document.writeln("gutom ako");
+}
